14c9e3d0960799b3f80a606685c14bac52df1f0e
Anyars Yussif <anyarsencarta@gmail.com>
2023-08-24 15:45:40 +0000

    Implement fetch from API

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,12 +1,93 @@
-// import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Quote.css';
 
+const uniqueID = 'a9QE8/PlmKOlgKno2cRt+w==r6HotYq9EvHchYmD';
+
 function Quote() {
+  const apiUrl = 'https://api.api-ninjas.com/v1/quotes';
+  const [quoteText, setQuoteText] = useState('');
+  const [authorText, setAuthor] = useState('');
+  const [categoryText, setCategory] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
+
+  useEffect(() => {
+    const fetchQuote = async () => {
+      try {
+        const response = await fetch(apiUrl, {
+          headers: {
+            'X-Api-Key': uniqueID,
+          },
+        });
+        if (!response.ok) {
+          throw new Error(
+            `API request failed with status code ${response.status}.`,
+          );
+        }
+        const data = await response.json();
+        setQuoteText(data[0].quote);
+        setAuthor(data[0].author);
+        setCategory(data[0].category);
+      } catch (error) {
+        setHasError(true);
+      }
+      setIsLoading(false);
+    };
+
+    fetchQuote();
+  }, []); // Empty dependency array to run only once on initial component mount
+
+  const fetchQuote = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(apiUrl, {
+        headers: {
+          'X-Api-Key': uniqueID,
+        },
+      });
+      if (!response.ok) {
+        throw new Error(
+          `API request failed with status code ${response.status}.`,
+        );
+      }
+      const data = await response.json();
+      setQuoteText(data[0].quote);
+      setAuthor(data[0].author);
+      setCategory(data[0].category);
+    } catch (error) {
+      setHasError(true);
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    const timer = setInterval(fetchQuote, 5000); // Change quote every 5 seconds (adjust as needed)
+
+    return () => clearInterval(timer); // Clear the timer on component unmount
+  }, []); // Empty dependency array to run only once on initial component mount
+
   return (
     <div className="quote-container">
       <h1 className="quote-header">Encarta Maths Magician - Quote of the Day!</h1>
-      <p className="quote">Sample Quote</p>
-      <p className="author-category">Author, Category</p>
+      {isLoading && <p className="loading">Loading Quote...</p>}
+      {hasError && <p className="error">Something went wrong!</p>}
+
+      {quoteText && (
+        <p className="quote">
+          &quot;
+          {quoteText}
+          &quot;
+        </p>
+      )}
+      <hr />
+      <br />
+      {authorText && (
+        <p className="author-category">
+          {authorText}
+          ,&nbsp;
+          {categoryText}
+        </p>
+      )}
     </div>
   );
 }
